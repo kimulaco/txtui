499adc071238c70e6d32b936579acee5590cf5bb
kimulaco <kimulaco@gmail.com>
2026-03-03 01:15:32 +0900

    update meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://txtui.dev",
   vite: {
     plugins: [/** @type {any} */ (tailwindcss())],
   },
